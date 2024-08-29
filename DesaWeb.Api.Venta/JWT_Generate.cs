f9af7df2bcd825434c62d1a75641aec3dac63c44
jhonathanixen <estuards52@gmail.com>
2024-08-28 21:50:59 -0600

    Initial commit

---
+++ DesaWeb.Api.Venta/JWT_Generate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Security.Cryptography;
+
+class JWT_Generate
+{
+    static void Main()
+    {
+        var key = new byte[32];
+        using (var generator = RandomNumberGenerator.Create())
+        {
+            generator.GetBytes(key);
+            string secret = Convert.ToBase64String(key);
+            Console.WriteLine(secret);
+        }
+    }
+}
---
+++ DesaWeb.Api.Venta/Program.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Configuración de JWT
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = "https://myapp.com",
+            ValidAudience = "https://myapp.com/audience",
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("dGhpc2lzYW5leGFtcGxlb2Zhc2VjdXJlYXV0aGVudGljYXRpb25rZXk="))
+        };
+    });
+
+
+// Añadir autenticación al pipeline
+var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
