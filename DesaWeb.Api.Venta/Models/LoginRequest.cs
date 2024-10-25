4f8a0028613a349d095f7eb2db72f8ef0aa377c0
jhonathanixen <estuards52@gmail.com>
2024-10-24 22:54:08 -0600

    integracion pendiente

---
+++ DesaWeb.Api.Venta/Models/LoginRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace DesaWeb.Api.Venta.Models
+{
+    public class LoginRequest
+    {
+        public string Correo { get; set; }
+        public string Contraseña { get; set; }
+    }
+
+}
---
+++ DesaWeb.Api.Venta/Models/Producto.cs
@@ -0,0 +1,15 @@
+﻿namespace DesaWeb.Api.Venta.Models
+{
+    public class Producto
+    {
+        public int ProductoID { get; set; }
+        public string CodigoProducto { get; set; }
+        public string Descripcion { get; set; }
+        public string CodigoProveedor { get; set; }
+        public DateTime FechaVencimiento { get; set; }
+        public string UbicacionFisica { get; set; }
+        public int ExistenciaMinima { get; set; }
+        public decimal Precio { get; set; }  // Nueva propiedad para el precio
+        public string Estado { get; set; } // 'V' para activo, 'I' para inactivo
+    }
+}
