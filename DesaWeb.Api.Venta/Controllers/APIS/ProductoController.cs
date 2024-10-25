4e7d5090910a61b9293873e0595c8ef86b4ae7f2
jhonathanixen <estuards52@gmail.com>
2024-10-24 23:05:24 -0600

    Revert "incorporacion clientes, entrega, productos, usuarios, ventas, proveedores"
    This reverts commit a7384ceffd641109099d0e157dc311719eb32d57.

---
+++ DesaWeb.Api.Venta/Controllers/APIS/ProductoController.cs
@@ -0,0 +1,179 @@
+﻿using DesaWeb.Api.Venta.Models;
+using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using System.Data;
+using System.Threading.Tasks;
+
+namespace DesaWeb.Api.Venta.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductoController : ControllerBase
+    {
+
+        private readonly MySQLDbContext _context;
+
+        public ProductoController(MySQLDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("crear-o-actualizar")]
+        public async Task<IActionResult> CrearOActualizarProducto([FromBody] Producto producto)
+        {
+            if (producto == null)
+                return BadRequest(new { mensaje = "Datos de producto inválidos" });
+
+            try
+            {
+
+                using var connection = _context.GetConnection();
+                await connection.OpenAsync();
+
+                using var command = new MySqlCommand("sp_crud_producto", connection)
+                {
+                    CommandType = CommandType.StoredProcedure
+                };
+
+                command.Parameters.AddWithValue("i_vCodigoProducto", producto.CodigoProducto);
+                command.Parameters.AddWithValue("i_vDescripcion", producto.Descripcion);
+                command.Parameters.AddWithValue("i_vCodigoProveedor", producto.CodigoProveedor);
+                command.Parameters.AddWithValue("i_dFechaVencimiento", producto.FechaVencimiento);
+                command.Parameters.AddWithValue("i_vUbicacionFisica", producto.UbicacionFisica);
+                command.Parameters.AddWithValue("i_iExistenciaMinima", producto.ExistenciaMinima);
+                command.Parameters.AddWithValue("i_dPrecio", producto.Precio);  // Nuevo parámetro para el precio
+                command.Parameters.AddWithValue("i_vEstado", producto.Estado);
+                command.Parameters.AddWithValue("i_vAccion", "C");  // 'C' para crear o actualizar
+                command.Parameters.AddWithValue("i_iProductoID", producto.ProductoID);
+
+                var result = await command.ExecuteNonQueryAsync();
+                return Ok(new { mensaje = "Producto creado o actualizado correctamente", filasAfectadas = result });
+            }
+            catch (MySqlException ex)
+            {
+                return StatusCode(500, new { mensaje = "Error al interactuar con la base de datos", error = ex.Message });
+            }
+
+        }
+
+        [HttpGet("listar-todo")]
+        public async Task<IActionResult> ListarTodosProductos()
+        {
+            try
+            {
+                using var connection = _context.GetConnection();
+                await connection.OpenAsync();
+
+                using var command = new MySqlCommand("sp_crud_producto", connection)
+                {
+                    CommandType = CommandType.StoredProcedure
+                };
+
+                // Parámetros para la acción 'T' (Listar todos los productos)
+                command.Parameters.AddWithValue("i_vAccion", "T"); // 'T' para leer todos los productos
+                command.Parameters.AddWithValue("i_iProductoID", DBNull.Value);
+                command.Parameters.AddWithValue("i_vCodigoProducto", DBNull.Value);
+                command.Parameters.AddWithValue("i_vDescripcion", DBNull.Value);
+                command.Parameters.AddWithValue("i_vCodigoProveedor", DBNull.Value);
+                command.Parameters.AddWithValue("i_dFechaVencimiento", DBNull.Value);
+                command.Parameters.AddWithValue("i_vUbicacionFisica", DBNull.Value);
+                command.Parameters.AddWithValue("i_iExistenciaMinima", DBNull.Value);
+                command.Parameters.AddWithValue("i_dPrecio", DBNull.Value); // Parámetro del precio
+                command.Parameters.AddWithValue("i_vEstado", DBNull.Value);
+
+                using var reader = await command.ExecuteReaderAsync();
+
+                var productos = new List<Producto>();
+
+                // Leer todos los productos
+                while (await reader.ReadAsync())
+                {
+                    var producto = new Producto
+                    {
+                        ProductoID = reader.GetInt32("ProductoID"),
+                        CodigoProducto = reader.GetString("CodigoProducto"),
+                        Descripcion = reader.GetString("Descripcion"),
+                        CodigoProveedor = reader.GetString("CodigoProveedor"),
+                        FechaVencimiento = reader.GetDateTime("FechaVencimiento"),
+                        UbicacionFisica = reader.GetString("UbicacionFisica"),
+                        ExistenciaMinima = reader.GetInt32("ExistenciaMinima"),
+                        Precio = reader.GetDecimal("Precio"), // Atributo del precio
+                        Estado = reader.GetString("Estado")
+                    };
+
+                    productos.Add(producto); // Agregar cada producto a la lista
+                }
+
+                if (productos.Count > 0)
+                {
+                    return Ok(productos); // Devolver la lista de productos
+                }
+                else
+                {
+                    return NotFound(new { mensaje = "No se encontraron productos disponibles" });
+                }
+            }
+            catch (MySqlException ex)
+            {
+                return StatusCode(500, $"Error al interactuar con la base de datos: {ex.Message}");
+            }
+        }
+
+
+        [HttpGet("leer/{id}")]
+        public async Task<IActionResult> LeerProducto(int id)
+        {
+            try
+            {
+
+                using var connection = _context.GetConnection();
+                await connection.OpenAsync();
+
+                using var command = new MySqlCommand("sp_crud_producto", connection)
+                {
+                    CommandType = CommandType.StoredProcedure
+                };
+
+                command.Parameters.AddWithValue("i_vAccion", "R"); // 'R' para leer
+                command.Parameters.AddWithValue("i_iProductoID", id);
+                command.Parameters.AddWithValue("i_vCodigoProducto", DBNull.Value);
+                command.Parameters.AddWithValue("i_vDescripcion", DBNull.Value);
+                command.Parameters.AddWithValue("i_vCodigoProveedor", DBNull.Value);
+                command.Parameters.AddWithValue("i_dFechaVencimiento", DBNull.Value);
+                command.Parameters.AddWithValue("i_vUbicacionFisica", DBNull.Value);
+                command.Parameters.AddWithValue("i_iExistenciaMinima", DBNull.Value);
+                command.Parameters.AddWithValue("i_dPrecio", DBNull.Value); // Nuevo parámetro para el precio
+                command.Parameters.AddWithValue("i_vEstado", DBNull.Value);
+
+                using var reader = await command.ExecuteReaderAsync();
+
+                if (reader.Read())
+                {
+                    var producto = new Producto
+                    {
+                        ProductoID = reader.GetInt32("ProductoID"),
+                        CodigoProducto = reader.GetString("CodigoProducto"),
+                        Descripcion = reader.GetString("Descripcion"),
+                        CodigoProveedor = reader.GetString("CodigoProveedor"),
+                        FechaVencimiento = reader.GetDateTime("FechaVencimiento"),
+                        UbicacionFisica = reader.GetString("UbicacionFisica"),
+                        ExistenciaMinima = reader.GetInt32("ExistenciaMinima"),
+                        Precio = reader.GetDecimal("Precio"), // Nuevo atributo para el precio
+                        Estado = reader.GetString("Estado")
+                    };
+                    return Ok(producto);
+                }
+                return NotFound("Producto no encontrado");
+            }
+            catch (MySqlException ex)
+            {
+                return StatusCode(500, new
+                {
+                    mensaje = "Error al interactuar con la base de datos",
+                    error = ex.Message
+                });
+            }
+
+        }
+    }
+}
---
+++ DesaWeb.Api.Venta/Controllers/APIS/VentaController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using System.Data;
+using System.Text.Json;
+using System.Threading.Tasks;
+using VentaModel = DesaWeb.Api.Venta.Models.Venta;
+
+namespace DesaWeb.Api.Venta.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class VentaController : ControllerBase
+    {
+
+        private readonly MySQLDbContext _context;
+
+        public VentaController(MySQLDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("registrar")]
+        public async Task<IActionResult> RegistrarVenta([FromBody] VentaModel venta)
+        {
+            if (venta == null || venta.DetalleVenta == null || venta.DetalleVenta.Count == 0)
+            return BadRequest(new { mensaje = "Datos de la venta o detalle de la venta inválidos" });
+
+            try
+            {
+                using var connection = _context.GetConnection();
+                await connection.OpenAsync();
+
+                using var command = new MySqlCommand("sp_registrar_venta", connection)
+                {
+                    CommandType = CommandType.StoredProcedure
+                };
+
+                command.Parameters.AddWithValue("i_iClienteID", venta.ClienteID);
+                command.Parameters.AddWithValue("i_vTipoVenta", venta.TipoVenta ?? (object)DBNull.Value);
+                command.Parameters.AddWithValue("i_dTotalVenta", venta.TotalVenta);
+                command.Parameters.AddWithValue("i_dFechaVenta", venta.FechaVenta);
+                command.Parameters.AddWithValue("i_vEstado", venta.Estado ?? (object)DBNull.Value);
+
+                // Convertir el detalle de la venta a formato JSON
+                var detalleJson = JsonSerializer.Serialize(venta.DetalleVenta);
+                command.Parameters.AddWithValue("i_tDetalleVenta", detalleJson);
+
+                var result = await command.ExecuteNonQueryAsync();
+                return Ok(new { mensaje = "Venta registrada correctamente", filasAfectadas = result });
+            }
+            catch (MySqlException ex)
+            {
+
+                return StatusCode(500, new
+                {
+                    mensaje = "Error al interactuar con la base de datos",
+                    error = ex.Message
+                });
+            }
+
+        }
+    }
+}
